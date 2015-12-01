c89d0e21e9a3cc74388bc3bce5c11d89c70e2dbb
wangyinxing <wangyinxing19@gmail.com>
2015-12-01 18:55:23 +0800

    Post method added.

--- src/ios/DCSync.m
+++ src/ios/DCSync.m
@@ -10,7 +10,52 @@
 #import <Cordova/CDV.h>
 #import "dcsync.h"
 
-@implementation DCSync
+#import "TextResponseSerializer.h"
+#import "HttpManager.h"
+
+@implementation DCSync {
+    AFHTTPRequestSerializer *requestSerializer;
+}
+
+
+- (void)setRequestHeaders:(NSDictionary*)headers {
+    [HttpManager sharedClient].requestSerializer = [AFHTTPRequestSerializer serializer];
+    [requestSerializer.HTTPRequestHeaders enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        [[HttpManager sharedClient].requestSerializer setValue:obj forHTTPHeaderField:key];
+    }];
+    [headers enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        [[HttpManager sharedClient].requestSerializer setValue:obj forHTTPHeaderField:key];
+    }];
+}
+
+
+
+- (void)post:(CDVInvokedUrlCommand*)command {
+    HttpManager *manager = [HttpManager sharedClient];
+    NSString *url = [command.arguments objectAtIndex:0];
+    NSDictionary *parameters = [command.arguments objectAtIndex:1];
+    NSDictionary *headers = [command.arguments objectAtIndex:2];
+    [self setRequestHeaders: headers];
+    
+    manager.responseSerializer = [TextResponseSerializer serializer];
+    [manager POST:url parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
+        [dictionary setObject:[NSNumber numberWithInt:operation.response.statusCode] forKey:@"status"];
+        [dictionary setObject:responseObject forKey:@"data"];
+        
+        
+        //CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:dictionary];
+        //[weakSelf.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
+        [dictionary setObject:[NSNumber numberWithInt:operation.response.statusCode] forKey:@"status"];
+        [dictionary setObject:[error localizedDescription] forKey:@"error"];
+        
+        
+        //CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:dictionary];
+        //[weakSelf.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+}
 
 
 
