d63263ac87ae5fc17fbe6bf3ba7b290620c184d4
wangyinxing <wangyinxing19@gmail.com>
2015-12-01 18:40:46 +0800

    Plugin.xml - added file list of HTTP Lib.
    AFNetworking Lib added...

---
+++ src/ios/DCSyncConst.h
@@ -0,0 +1,26 @@
+//
+//  DCSyncConst.h
+//  app
+//
+//  Created by ACE on 12/1/15.
+//
+//
+
+#ifndef DCSyncConst_h
+#define DCSyncConst_h
+
+
+#define DCSYNC_WSE_URL      @"https://ch-co2tieferlegen.preview.kju.at"
+
+
+
+/*####################################################################################################
+ Backend entries......
+####################################################################################################*/
+
+#define DCSYNC_WSE_AUTH      @"/dc/accesstoken"
+#define DCSYNC_WSE_SYNC      @"/dc/sync"
+
+
+
+#endif /* DCSyncConst_h */
